177cd0cdf654ff8755c618d556e99e8ad6e7a597
Ilyas Akramov <98989079+IRENDD@users.noreply.github.com>
2024-02-15 20:00:49 +0800

    update 3

--- lib/features/location/screens/explore/explore.dart
+++ lib/features/location/screens/explore/explore.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:flutter_application_1/common/widgets/custom_shapes/shapes/drage_handle.dart';
-import 'package:flutter_application_1/common/widgets/events/event_cards/event_card_horizontal.dart';
 import 'package:flutter_application_1/common/widgets/images/t_circular_image.dart';
-import 'package:flutter_application_1/common/widgets/texts/section_heading.dart';
+import 'package:flutter_application_1/features/location/screens/explore/widgets/event_section.dart';
 import 'package:flutter_application_1/map.dart';
 import 'package:flutter_application_1/utils/constants/colors.dart';
 import 'package:flutter_application_1/utils/constants/image_string.dart';
@@ -17,143 +16,105 @@ class ExploreScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          /// -- Map
+          /// Map
           SizedBox(
             width: double.infinity,
             height: MediaQuery.of(context).size.height,
             child: const MapScreen(),
           ),
 
-          /// -- Draggable Scrollable Sheet
+          /// Draggable Scrollable Sheet
           DraggableScrollableSheet(
-            // The initial size of the sheet when the app starts
             initialChildSize: 0.4,
-            // The minimum height your sheet can shrink to
             minChildSize: 0.3,
-            // The maximum size the bottom sheet can expand to.
             maxChildSize: 0.95,
-
             builder: (BuildContext context, ScrollController scrollController) {
               return Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: TColors.white,
                   borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(20),
                     topRight: Radius.circular(20),
                   ),
                   boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      color: TColors.darkShadow,
-                    ),
+                    BoxShadow(blurRadius: 10, color: TColors.darkShadow)
                   ],
                 ),
-
-                /// -- Main Page
-                child: SingleChildScrollView(
+                child: CustomScrollView(
                   controller: scrollController,
-                  child: Column(
-                    children: [
-                      /// -- Drage Handle Icon
-                      TDragHandle(color: TColors.textGrey.withOpacity(0.65)),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: TSizes.md, vertical: TSizes.sm),
-                        child: Column(
-                          children: [
-                            const Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                    child:
-                                        TSeachContainer(text: 'Search in Map')),
-                                SizedBox(width: TSizes.spaceBtwItems),
-                                TCircularImage(
-                                  image: TImages.user,
-                                  width: 40,
-                                  height: 40,
-                                  padding: 0,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: TSizes.spaceBtwSections),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: TSizes.md),
-                              child: Column(
-                                children: [
-                                  const TSectionHeading(
-                                    title: 'Upcoming Events',
-                                    showActionButton: true,
-                                    textColor: TColors.secondary,
-                                  ),
-                                  const SizedBox(height: TSizes.spaceBtwItems),
-                                  SizedBox(
-                                    height: 170,
-                                    child: ListView.separated(
-                                      separatorBuilder: (context, index) =>
-                                          const SizedBox(
-                                              width:
-                                                  TSizes.spaceBtwItems * 1.5),
-                                      itemCount: 4,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: (context, index) =>
-                                          (const TEventCardHorizontal()),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                      height: TSizes.spaceBtwSections),
-                                  const TSectionHeading(
-                                    title: 'Upcoming Events',
-                                    showActionButton: true,
-                                    textColor: TColors.secondary,
-                                  ),
-                                  const SizedBox(height: TSizes.spaceBtwItems),
-                                  SizedBox(
-                                    height: 170,
-                                    child: ListView.separated(
-                                      separatorBuilder: (context, index) =>
-                                          const SizedBox(
-                                              width:
-                                                  TSizes.spaceBtwItems * 1.5),
-                                      itemCount: 4,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: (context, index) =>
-                                          (const TEventCardHorizontal()),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                      height: TSizes.spaceBtwSections),
-                                  const TSectionHeading(
-                                    title: 'Upcoming Events',
-                                    showActionButton: true,
-                                    textColor: TColors.secondary,
-                                  ),
-                                  const SizedBox(height: TSizes.spaceBtwItems),
-                                  SizedBox(
-                                    height: 170,
-                                    child: ListView.separated(
-                                      separatorBuilder: (context, index) =>
-                                          const SizedBox(
-                                              width:
-                                                  TSizes.spaceBtwItems * 1.5),
-                                      itemCount: 4,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: (context, index) =>
-                                          (const TEventCardHorizontal()),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                      height: TSizes.spaceBtwSections),
-                                ],
-                              ),
-                            ),
-                          ],
+                  slivers: <Widget>[
+                    SliverAppBar(
+                      pinned: true,
+                      backgroundColor: TColors
+                          .white, // Ensures the app bar background color.
+                      shape: const ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(42),
+                          topRight: Radius.circular(42),
                         ),
                       ),
-                      // You can add more widgets here as needed
-                    ],
-                  ),
+                      flexibleSpace: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          // This is used to apply effects or changes when the app bar is expanded or collapsed
+                          return FlexibleSpaceBar(
+                            background: Container(
+                              decoration: BoxDecoration(
+                                color: TColors.white,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 10, color: TColors.darkShadow)
+                                ],
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: TSizes.md, vertical: TSizes.sm),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TDragHandle(
+                                        color:
+                                            TColors.textGrey.withOpacity(0.65)),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Expanded(
+                                          child: TSeachContainer(
+                                              text: 'Search in Map'),
+                                        ),
+                                        SizedBox(width: TSizes.spaceBtwItems),
+                                        TCircularImage(
+                                          image: TImages.user,
+                                          width: 40,
+                                          height: 40,
+                                          padding: 0,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SliverToBoxAdapter(
+                      child: Column(
+                        children: [
+                          EventSection(),
+                          EventSection(),
+                          EventSection(),
+                          EventSection(),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
